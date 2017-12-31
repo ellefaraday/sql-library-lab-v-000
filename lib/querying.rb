bd5e32a3a603101f5ec8f3b19f2de6d0ce36d55f
Elle Faraday <faraday.elle@gmail.com>
2017-12-31 21:36:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN books ON books.series_id = series.id JOIN character_books where character_books.book_id = books.id JOIN characters ON characters.id = character_books.character_id GROUP BY character.species ORDER BY COUNT(characters.species) DESC LIMIT 1 WHERE characters.species = 'human' "
+  "SELECT series.title FROM series JOIN books ON books.series_id = series.id JOIN character_books ON character_books.book_id = books.id JOIN characters ON characters.id = character_books.character_id GROUP BY character.species ORDER BY COUNT(characters.species) DESC LIMIT 1 WHERE characters.species = 'human' "
 end
 
 def select_character_names_and_number_of_books_they_are_in
